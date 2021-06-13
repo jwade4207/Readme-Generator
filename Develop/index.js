50c12a03beec707439dfd0113003f1c776152813
jwade4207 <jwade4207@yahoo.com>
2021-06-13 18:30:16 -0500

    save changes

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,113 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
+const generateMarkdown = require('./utils/generateMarkdown.js')
+
+// });
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project? (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('You can not proceed until you have entered the title of your project.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'contributer', 
+            message: 'Did you contribute on this project? If so, please enter your first and last name. If not, please skip this question.'
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is you Github username? (Required)',
+            validate: userNameInput => {
+                if (userNameInput) {
+                    return true;
+                } else {
+                    console.log('You can not proceed until you have entered your Github username.');
+                    return false;
+                }
+            }
+        
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email address.'
+        },
+        {
+            type: 'input', 
+            name: 'description',
+            message: 'Provide a description of this project. (Required)', 
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('You can not proceed until you have entered a description.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Please enter any installization instructions.',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide usage information for your project.',
+            default: true
+        },
+        {
+            type: 'list',
+            name: 'licenses',
+            message: 'What licenses did you use? (Check all that apply)',
+            choices: ['General Public License', 'Apache', 'Microsoft Public License', 'Berkeley Software Distrobution', 'Eclipse Public License']
+        }
+    ])
+};
+
+//questions().then(answers => console.log(answers));
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// writeToFile = (fileName, data) => {
+    
+//     fs.writeFile('.README.md', generateMarkdown, err => {
+//         if (err) throw err;
+
+//         console.log('ReadMe generated!');
+//     });
+// };
 
 // TODO: Create a function to initialize app
-function init() {}
+init = () => { 
+    questions().then((response) => {
+        //console.log(response)
+        
+        const answersString = generateMarkdown(response);
+        
+        //console.log(answersString)
+
+        fs.writeFile('.README.md', answersString, err => {
+            if (err) throw err;
+
+            console.log('ReadMe Generated!');
+        })
+    })
+}
 
 // Function call to initialize app
-init();
+init()
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,60 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const renderLicenseBadge = licenseInput => {
+  if(!licenseInput) {
+    return ''
+  }
+  return `<img src="https://img.shields.io/badge/License-${licenseInput}-green" alt="badge with license name"/>`
 
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const renderLicenseLink = licenseLink => {
+  if(!licenseLink) {
+  return ''
+  }
+  return ''
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const renderLicenseSection = license => {
+
+  if(!license) {
+    return ''
+  }
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+const generateMarkdown = data => {
   return `# ${data.title}
-
+  
+  ## Description 
+  ${data.description}
+  
+  ## Table of Contents
+  
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  
+  ## Usage 
+    
+  ## License
+  ${renderLicenseBadge(data.licenses)}
+  
+  ## Contributing
+  ${data.contributer}
+  
+  ## Tests
+  
+  ## Questions
+  If you have any questions, please feel free to visit my Github Account at [${data.username}](https://github.com/${data.username}), or shoot me an email here: ${data.email}
 `;
 }
 
